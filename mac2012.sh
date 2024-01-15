5401ca3e52b9cbea53d4cdcb873afb1850daca94
bob <billy@gmail.com>
2024-01-15 16:19:55 -0600

    testing the removal of backups

--- mac2012.sh
+++ mac2012.sh
@@ -38,7 +38,7 @@ sudo apt-get install sublime-text -y
 subl
 
 log "Configuring sublime"
-backup ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
+#backup ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
 
 echo '// and are overridden in turn by syntax-specific settings.
 {
@@ -49,7 +49,7 @@ echo '// and are overridden in turn by syntax-specific settings.
   "trim_trailing_white_space_on_save": "all",
 }' >> ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
 
-backup ~/.config/sublime-text/Packages/User/Default.sublime-keymap
+#backup ~/.config/sublime-text/Packages/User/Default.sublime-keymap
 
 echo '[
   { "keys": ["ctrl+left"], "command": "move_to", "args": {"to": "bol", "extend": false} },
@@ -78,7 +78,7 @@ xmodmap ~/.Xmodmap
 
 log "fixing the trackpad speed, tap-to-click, and reverse scrolling"
 
-backup ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
+#backup ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
 
 # To fix the trackpad speed, tap-to-click, and reverse scrolling:
 sed -i 's|^.*name="Acceleration".*$|    <property name="Acceleration" type="double" value="4.200000"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
@@ -94,7 +94,7 @@ log "fixing cycle_windows_key, cycle_reverse_windows_key, and switch_window_key"
 sudo dpkg-reconfigure keyboard-configuration
 # open Window Manager / Keyboard tab and set:
 #   Cycle windows(223) Ctrl+Tab:
-backup ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml{.bak}
+#backup ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml{.bak}
 
 sed -i 's|^.* value="cycle_windows_key".*$|      <property name="\&lt;Primary\&gt;Tab" type="string" value="cycle_windows_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 #   Cycle windows Reverse(225) Shift+Ctrl+LeftTab:
@@ -106,7 +106,7 @@ log "load Xmodmap"
 xmodmap ~/.Xmodmap
 
 log "configure terminal copy&paste shortcuts"
-backup ~/.config/xfce4/terminal/accels.scm
+#backup ~/.config/xfce4/terminal/accels.scm
 
 # terminal
 # https://forum.xfce.org/viewtopic.php?id=12105
@@ -126,8 +126,8 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global user.email "billy@gmail.com"
-git config --global user.name "bob"
+git config --global user.email "$(whoami)@xubuntu"
+git config --global user.name "$(whoami)"
 ssh-keygen -t ed25519 -C "$(whoami)@xubuntu" -N "" -f ~/.ssh/id_ed25519
 
 # picture editor
