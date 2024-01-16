5e3ed72415f7d77b1f1ff80f88dfa5c5c2f90954
bob <billy@gmail.com>
2024-01-15 18:18:58 -0600

    shuffled things around

--- mac2012.sh
+++ mac2012.sh
@@ -29,10 +29,54 @@ sudo apt remove xserver-xorg-input-synaptics
 log "Wifi"
 sudo apt install bcmwl-kernel-source -y
 
-log "Installing sublime"
+log "fixing the theme and stuff"
+sed -i 's|^.*name="ThemeName".*$|    <property name="ThemeName" type="string" value="Adwaita-dark"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+sed -i 's|^.*name="IconThemeName".*$|    <property name="IconThemeName" type="string" value="elementary-xfce-darker"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+
+##########################
+# Mac Keyboard mapping
+##########################
+
+log "installing xmodmap"
+sudo apt install xkeycaps -y
+xmodmap -pke > ~/.Xmodmap
+log "fixing Ctrl to Cmd for shortcuts"
+echo "! bind left command key as another Ctrl key
+remove mod4 = Super_L
+keysym Super_L = Control_L
+add Control = Control_L" >> ~/.Xmodmap
+log "load Xmodmap"
+xmodmap ~/.Xmodmap
+
+log "fixing the trackpad speed, tap-to-click, and reverse scrolling"
+sed -i 's|^.*name="Acceleration".*$|    <property name="Acceleration" type="double" value="4.200000"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
+sed -i 's|^.*name="libinput_Tapping_Enabled".*$|      <property name="libinput_Tapping_Enabled" type="int" value="1"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
+sed -i 's|^.*name="ReverseScrolling".*$|    <property name="ReverseScrolling" type="bool" value="true"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
+
+# select macbook pro (not sure this is necessary):
+# sudo dpkg-reconfigure keyboard-configuration
+
+log "fixing cycle_windows_key, cycle_reverse_windows_key, and switch_window_key"
+# manually: open Window Manager / Keyboard tab and set
+#   Cycle windows(223) Ctrl+Tab:
+sed -i 's|^.* value="cycle_windows_key".*$|      <property name="\&lt;Primary\&gt;Tab" type="string" value="cycle_windows_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+#   Cycle windows Reverse(225) Shift+Ctrl+LeftTab:
+sed -i 's|^.* value="cycle_reverse_windows_key".*$|      <property name="\&lt;Primary\&gt;\&lt;Shift\&gt;ISO_Left_Tab" type="string" value="cycle_reverse_windows_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+#   Switch window for same app Ctrl+`:
+sed -i 's|^.* value="switch_window_key".*$|      <property name="\&lt;Primary\&gt;grave" type="string" value="switch_window_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
+log "configure terminal copy&paste shortcuts"
+# terminal
+# https://forum.xfce.org/viewtopic.php?id=12105
+# https://unix.stackexchange.com/questions/271150/how-to-set-ctrlc-to-copy-ctrlv-to-paste-and-ctrlshiftc-to-kill-process-in-x
+# subl ~/.config/xfce4/terminal/accels.scm
+echo '(gtk_accel_path "<Actions>/terminal-window/copy" "<Primary>c")
+(gtk_accel_path "<Actions>/terminal-window/paste" "<Primary>v")' >> ~/.config/xfce4/terminal/accels.scm
+
 #################
 # Sublime
 #################
+log "Installing sublime"
 # https://www.sublimetext.com/docs/linux_repositories.html
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -64,56 +108,11 @@ echo '[
   { "keys": ["ctrl+g"], "command": "find_next" },
 ]' >> ~/.config/sublime-text/Packages/User/Default.sublime-keymap
 
-log "installing xmodmap"
-sudo apt install xkeycaps -y
-xmodmap -pke > ~/.Xmodmap
-
-log "fixing Ctrl to Cmd for shortcuts"
-echo "! bind left command key as another Ctrl key
-remove mod4 = Super_L
-keysym Super_L = Control_L
-add Control = Control_L" >> ~/.Xmodmap
-xmodmap ~/.Xmodmap
-
-log "fixing the trackpad speed, tap-to-click, and reverse scrolling"
-sed -i 's|^.*name="Acceleration".*$|    <property name="Acceleration" type="double" value="4.200000"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
-sed -i 's|^.*name="libinput_Tapping_Enabled".*$|      <property name="libinput_Tapping_Enabled" type="int" value="1"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
-sed -i 's|^.*name="ReverseScrolling".*$|    <property name="ReverseScrolling" type="bool" value="true"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
-
-##########################
-# Mac Keyboard mapping
-##########################
-log "fixing cycle_windows_key, cycle_reverse_windows_key, and switch_window_key"
-
-# select macbook pro (not sure this is necessary):
-# sudo dpkg-reconfigure keyboard-configuration
-# open Window Manager / Keyboard tab and set:
-#   Cycle windows(223) Ctrl+Tab:
-
-sed -i 's|^.* value="cycle_windows_key".*$|      <property name="\&lt;Primary\&gt;Tab" type="string" value="cycle_windows_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-#   Cycle windows Reverse(225) Shift+Ctrl+LeftTab:
-sed -i 's|^.* value="cycle_reverse_windows_key".*$|      <property name="\&lt;Primary\&gt;\&lt;Shift\&gt;ISO_Left_Tab" type="string" value="cycle_reverse_windows_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-#   Switch window for same app Ctrl+`:
-sed -i 's|^.* value="switch_window_key".*$|      <property name="\&lt;Primary\&gt;grave" type="string" value="switch_window_key"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-
-log "load Xmodmap"
-xmodmap ~/.Xmodmap
-
-log "configure terminal copy&paste shortcuts"
-
-# terminal
-# https://forum.xfce.org/viewtopic.php?id=12105
-# https://unix.stackexchange.com/questions/271150/how-to-set-ctrlc-to-copy-ctrlv-to-paste-and-ctrlshiftc-to-kill-process-in-x
-# subl ~/.config/xfce4/terminal/accels.scm
-echo '(gtk_accel_path "<Actions>/terminal-window/copy" "<Primary>c")
-(gtk_accel_path "<Actions>/terminal-window/paste" "<Primary>v")' >> ~/.config/xfce4/terminal/accels.scm
-
 #################
-# Git
+# useful software
 #################
 log "install git"
 sudo apt install git -y
-
 log "set git aliases"
 git config --global alias.co checkout
 git config --global alias.br branch
@@ -121,21 +120,11 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global user.email "$(whoami)@xubuntu"
 git config --global user.name "$(whoami)"
-ssh-keygen -t ed25519 -C "$(whoami)@xubuntu" -N "" -f ~/.ssh/id_ed25519
 
-# picture editor
-# sudo apt-get install gthumb
-
-# webcam photos
-# sudo apt-get install cheese
-
-# chrome
 log "installing chrome"
 # found the url in https://www.google.com/chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
-
-# sudo apt install supertux
 
 log "installing Bluetooth"
 sudo apt install bluez* -y
@@ -144,13 +133,10 @@ sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 sudo rfkill unblock bluetooth
 
-log "fixing the theme and stuff"
-sed -i 's|^.*name="ThemeName".*$|    <property name="ThemeName" type="string" value="Adwaita-dark"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
-sed -i 's|^.*name="IconThemeName".*$|    <property name="IconThemeName" type="string" value="elementary-xfce-darker"/>|' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
-
 sudo apt install virt-manager -y
 # sudo mv xubuntu-22.04.3-desktop-amd64.iso /var/local
 
+ssh-keygen -t ed25519 -C "$(whoami)@xubuntu" -N "" -f ~/.ssh/id_ed25519
 log "remember to upload the following key to your github account:"
 cat ~/.ssh/id_ed25519.pub
 
@@ -173,6 +159,15 @@ echo "sudo reboot"
 ################
 # Useful
 ################
+
+# picture editor
+# sudo apt-get install gthumb
+
+# webcam photos
+# sudo apt-get install cheese
+
+# sudo apt install supertux
+
 
 # CHANGE DISK ENCRYPTION PASSWORD
 # cat /etc/crypttab
