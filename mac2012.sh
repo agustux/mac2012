e57eb8f890bc2a844fbeac974b42749fd19df850
bob <billy@gmail.com>
2024-01-16 21:19:41 -0600

    fixed pwd for chrome deb file

--- mac2012.sh
+++ mac2012.sh
@@ -130,7 +130,7 @@ git config --global user.name "$(whoami)"
 log "installing chrome"
 # found the url in https://www.google.com/chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+sudo dpkg -i $SCRIPT_DIR/google-chrome-stable_current_amd64.deb
 
 log "installing Bluetooth"
 sudo apt install bluez* -y
