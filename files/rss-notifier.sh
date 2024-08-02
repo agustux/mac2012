ebc50f0375b1be97971552b3b15138affb6f9fdb
gus <gus@ubuntu>
2024-08-02 16:34:47 -0500

    experimental rss tasks

---
+++ files/rss-notifier.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -o /tmp/rss-output-source.txt
+diff /tmp/rss-output-source.txt /tmp/rss-output-target.txt
+EXIT_STATUS=$?
+cp /tmp/rss-output-source.txt /tmp/rss-output-target.txt
+[ "$EXIT_STATUS" -ne 0 ] && exit 1
